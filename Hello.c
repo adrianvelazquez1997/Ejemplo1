8581bc33a25a34eb118bb505a269d705173e7d6f
adrianvelazquez.unrn@gmail.com <adrianvelazquez.unrn@gmail.com>
2024-01-29 18:23:28 -0300

    doc1

--- Hello.c
+++ Hello.c
@@ -1,4 +1,9 @@
 int main(){
     pirntf("Hello World")};
+    // git clone <repo>
+    // git fetch
+    // git pull origin main
+    // git commit -m "mensaje"
+    // git push orign main
 }
 retrun 0;
